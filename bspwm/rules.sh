f5642136021e093c21f585c72c67f0c8abc4d467
Amirreza H <itsamirrezah@gmail.com>
2023-02-21 15:12:11 +0330

    basic setup for bspwm

---
+++ bspwm/rules.sh
@@ -0,0 +1,8 @@
+# Floating Applications
+bspc rule -a Arandr state=floating
+bspc rule -a Archlinux-tweak-tool.py state=floating
+bspc rule -a Arcolinux-welcome-app.py state=floating
+bspc rule -a Nitrogen state=floating
+bspc rule -a rofi state=floating sticky=on
+bspc rule -a Windscribe2 state=floating desktop="^10"
+
