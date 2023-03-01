26594af787a8f53bea37211367af0152f0241f12
Amireza H <itsamirrezah@gmail.com>
2023-03-01 14:53:08 +0330

    bspwm-autostart updated

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -2,7 +2,7 @@
 
 #Arandr
 #NOTE: Save your configuration via arandr into ~/.screenlayout/monitor.sh
-sh ~/.screenlayout/monitor.sh &;
+sh ~/.screenlayout/monitor.sh &
 
 #Picom Configuration
 picom --config $HOME/.config/bspwm/picom.conf &
@@ -11,18 +11,20 @@ picom --config $HOME/.config/bspwm/picom.conf &
 dex -a -s /etc/xdg/autostart:~/.config/autostart/ &
 
 #Cursor
-xsetroot -cursor_name left_ptr;
+xsetroot -cursor_name left_ptr
 
 #Restore Background
-nitrogen --restore &;
+nitrogen --restore &
 
 # Network & Bluetooth Tray 
-nm-applet &;
-blueberry-tray &;
+nm-applet &
+blueberry-tray &
 
 # Pollybar
-$HOME/.config/bspwm/polybar/launch.sh &;
+$HOME/.config/bspwm/polybar/launch.sh &
 
 #Keyboard Configuration
-sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &;
+sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
+#Polkit
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
