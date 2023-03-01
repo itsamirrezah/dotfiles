8eab3ec9335a349019e09c747891453bf529d32c
Amireza H <itsamirrezah@gmail.com>
2023-03-01 17:20:13 +0330

    polybar fixed

--- bspwm/launcher/launcher.sh
+++ bspwm/launcher/launcher.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+
 THEME_1="simple"
 THEME_2="dark"
-if [ "$1" = "$THEME_1" ];
+if [ "$1" = "$THEME_1" ]
 then
 	rofi -modi drun -show window -line-padding 4 \
 		-columns 2 -padding 50 -hide-scrollbar \
-		-show-icons -drun-icon-theme "Qogir-dark" -font "Droid Sans Regular 10"
+		-show-icons -drun-icon-theme "Qogir-dark" -font "Droid Sans Regular 10";
 
-elif [ "$1" = "$THEME_2" ];
-	rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/bspwm/launcher/rofi/theme-dark.rasi
+elif [ "$1" = "$THEME_2" ]
+then
+	rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/bspwm/launcher/rofi/theme-dark.rasi;
 fi
 
 
