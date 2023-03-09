dac1e027c1eafe653359b2044aa31f7ab388c863
Amirreza H <itsamirrezah@gmail.com>
2023-03-09 16:37:54 +0330

    change keyboard layout via capslock

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -1,3 +1,6 @@
+echo "install dependencies..."
 sudo pacman -S - < requirement.txt
-
+echo "copy configuration"
 cp -r . ~/.config/bspwm/.
+echo "remap caps lock: change keyboard layout"
+sudo localectl set-x11-keymap us "grp:caps_toggle"
