83c4856f16b3a3f2dece63d03992513125125b7c
Amirreza H <itsamirrezah@gmail.com>
2023-03-14 17:59:08 +0330

    toggle picom script

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -2,5 +2,6 @@ echo "install dependencies..."
 sudo pacman -S - < requirement.txt
 echo "copy configuration"
 cp -r . ~/.config/bspwm/.
+cp -r ../scripts ~/.config/.
 echo "remap caps lock: change keyboard layout"
 sudo localectl set-x11-keymap us "grp:caps_toggle"
