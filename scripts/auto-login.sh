a574e72e2ac34ed617731ef3e524e41a97ba5fde
Amirreza H <itsamirrezah@gmail.com>
2023-03-15 18:51:49 +0330

    basic dunst config

--- scripts/auto-login.sh
+++ scripts/auto-login.sh
@@ -4,6 +4,6 @@ groupadd -r autologin
 #add current user to autologin group
 sudo gpasswd -a $USER autologin
 #lightdm configs
-sudo sed -i 's/'#autologin-user='/'autologin-user=USER'/g' /etc/lightdm/lightdm.conf
+sudo sed -i 's/'#autologin-user='/'autologin-user=$USER'/g' /etc/lightdm/lightdm.conf
 sudo sed -i 's/'#autologin-session='/'autologin-session=$session'/g' /etc/lightdm/lightdm.conf
 echo "done!"
