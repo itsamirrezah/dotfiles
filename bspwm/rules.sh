a83113e09e8214f95ece1c1346aa926ba0e1c63b
Amirreza H <itsamirrezah@gmail.com>
2023-03-18 00:27:25 +0330

    add desktop rules

--- bspwm/rules.sh
+++ bspwm/rules.sh
@@ -4,7 +4,24 @@ bspc rule -a Archlinux-tweak-tool.py state=floating
 bspc rule -a Arcolinux-welcome-app.py state=floating
 bspc rule -a Nitrogen state=floating
 bspc rule -a rofi state=floating sticky=on
-bspc rule -a Windscribe2 state=floating desktop="^10"
 bspc rule -a feh state=floating
 bspc rule -a Galculator state=floating
-
+bspc rule -a Blueberry.py state=floating
+bspc rule -a Pavucontrol state=floating
+#==================================================
+# Desktop Rules
+bspc rule -a Brave-browser:*:Todoist desktop="^1" follow=on focus=on
+bspc rule -a Brave-browser desktop="^2" follow=on focus=on
+bspc rule -a Code desktop="^3" follow=on focus=on
+bspc rule -a VSCodium desktop="^4" follow=on focus=on
+bspc rule -a discord desktop="^5" follow=on focus=on
+bspc rule -a TelegramDesktop desktop="^5" follow=on focus=on
+bspc rule -a thunderbird desktop="^5" follow=on focus=on
+bspc rule -a Element desktop="^5" follow=on focus=on
+bspc rule -a obsidian desktop="^6" follow=on focus=on
+bspc rule -a Pcmanfm desktop="^6" follow=on focus=on
+bspc rule -a LibreWolf desktop="^6" follow=on focus=on
+bspc rule -a streamlink-twitch-gui desktop="^6" follow=on focus=on
+bspc rule -a qBittorrent desktop="^7" follow=on focus=on
+bspc rule -a Brave-browser:*:Spotify desktop="^7" follow=on focus=on
+bspc rule -a Windscribe2 state=floating desktop="^7" border=off layer=below
