d0a0c9297529f49841bad9a475852fed6ce9cdff
Amirreza H <itsamirrezah@gmail.com>
2023-03-24 03:32:48 +0330

    minor changes

--- bspwm/launcher/launcher.sh
+++ bspwm/launcher/launcher.sh
@@ -4,7 +4,7 @@ THEME_1="simple"
 THEME_2="dark"
 if [ "$1" = "$THEME_1" ]
 then
-	rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/bspwm/launcher/rofi/blue-dark.rasi;
+	rofi -terminal alacritty -sensible-terminal -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/bspwm/launcher/rofi/blue-dark.rasi;
 elif [ "$1" = "$THEME_2" ]
 then
 	rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/bspwm/launcher/rofi/theme-dark.rasi;
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -77,7 +77,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-autosuggestions zsh-autocomplete vi-mode colored-man-pages zsh-syntax-highlighting)
+plugins=(git zsh-autosuggestions zsh-autocomplete vi-mode zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
