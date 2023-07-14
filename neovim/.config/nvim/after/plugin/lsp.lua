919c89cf9e2223ec9ea7896f8cc0e5e6d30bec3b
Amirreza H <itsamirrezah@gmail.com>
2023-07-14 22:55:02 +0330

    add remaps for lspzero & trouble

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -1,13 +1,20 @@
 require("neodev").setup({})
--- require("neoconf").setup({})
 
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr, preserve_mappings = false})
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>gl", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set({ "v", "n" }, "<leader>ff", function() vim.lsp.buf.format() end, opts)
 end)
 
-lsp.setup_servers({'tsserver', 'eslint'})
+lsp.setup_servers({ 'tsserver', 'eslint' })
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
