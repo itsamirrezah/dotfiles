f824370a1e9660adc5a4b601b42d5eeb10effd08
Amirreza H. <itsamirrezah@gmail.com>
2023-08-31 21:26:20 +0330

    update bspwm dependencies

--- scripts/.local/bin/install-scripts/bspwm.sh
+++ scripts/.local/bin/install-scripts/bspwm.sh
@@ -1,11 +1,1 @@
-echo "install dependencies..."
-sudo pacman -S - < requirement.txt
-echo "copy configuration"
-cp -r . ~/.config/bspwm/.
-cp -r ../polybar ~/.config/.
-cp -r ../rofi ~/.config/.
-cp -r ../picom ~/.config/.
-cp -r ../scripts ~/.config/.
-cp -r ../dunst ~/.config/.
-echo "remap caps lock: change keyboard layout"
-sudo localectl set-x11-keymap us "grp:caps_toggle"
+sudo pacman -S - < $HOME/.config/bspwm/requirement.txt
