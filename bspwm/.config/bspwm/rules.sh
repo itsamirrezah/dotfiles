ca9516911c3096a624f1e8fec653ce7804079c36
Amirreza H. <itsamirrezah@gmail.com>
2024-01-06 22:13:39 +0330

    migrate alacritty config and update shell

--- bspwm/.config/bspwm/rules.sh
+++ bspwm/.config/bspwm/rules.sh
@@ -18,12 +18,11 @@ bspc rule -a discord desktop="^5" follow=on focus=on
 bspc rule -a TelegramDesktop desktop="^5" follow=on focus=on
 bspc rule -a thunderbird desktop="^5" follow=on focus=on
 bspc rule -a Element desktop="^5" follow=on focus=on
-bspc rule -a obsidian desktop="^6" follow=on focus=on
 bspc rule -a Pcmanfm desktop="^6" follow=on focus=on
 bspc rule -a LibreWolf desktop="^6" follow=on focus=on
 bspc rule -a streamlink-twitch-gui desktop="^6" follow=on focus=on
 bspc rule -a qBittorrent desktop="^8" follow=on focus=on
-bspc rule -a Brave-browser:*:Spotify desktop="^8" follow=on focus=on
+bspc rule -a Brave-browser:*:Spotify desktop="^8" follow=on focus=on state=floating
 bspc rule -a Blueberry.py state=floating desktop="^8" follow=on focus=on
 bspc rule -a Pavucontrol state=floating desktop="^8" follow=on focus=on
 bspc rule -a Windscribe2 state=floating desktop="^8" border=off layer=below
